b2fc160fc5e1feda36df307116deda99768b57f5
sunFlowerB <1275460914@qq.com>
2020-01-06 18:06:39 +0800

    开发环境

---
+++ src/main/java/com/youzhong/entity/Dept.java
@@ -0,0 +1,6 @@
+package com.youzhong.entity;
+
+public class Dept {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/youzhong/entity/Emp.java
@@ -0,0 +1,6 @@
+package com.youzhong.entity;
+
+public class Emp {
+    private int id;
+    private String name;
+}
